8ff35392da38ae708efa43d0359f83786c7f6a73
boydlakers15 <jboyd115@icloud.com>
2023-05-03 12:34:10 +0200

    changed the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default {
+  plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-js-pagination']
+    }
+  }
+}
