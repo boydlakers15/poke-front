6495c43351976b3c2269a78df018023129d95427
boydlakers15 <jboyd115@icloud.com>
2023-05-04 09:07:30 +0200

    made changes to the logo

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
     <nav className="nav">
       <p className="section">
-      <div>
+      <div className='logo'>
         <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Pok%C3%A9mon_TCG_Online_Logo.png" alt="Pokemon TCG Online Logo" className="logo" />
       </div>
           <center> <img src="https://server.emulator.games/images/gameboy-color/pokemon-blue-version-ua.jpg" alt="Pokemon Blue Version" width="350" height="350" /></center>
